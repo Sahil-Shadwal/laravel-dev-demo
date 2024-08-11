3883a67db0e0f5fdb80435193277d377cd78c8c7
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-08-11 14:08:25 +0530

    layout

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -4,11 +4,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact</title>
+    <title>Home</title>
 </head>
 
 <body>
-    <h2>U can reach out whenever you like :)</h2>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+    </nav>
+
+    <?php echo $slot ?>
 </body>
 
 </html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <h1> Welcome to home!</h1>
+
+</x-layout>
