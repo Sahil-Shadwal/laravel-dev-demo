72b071dfd03011af2acd33d63ea268f207331124
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-08-12 23:05:23 +0530

    migration from route closer to dedicated controller action

--- routes/web.php
+++ routes/web.php
@@ -1,85 +1,9 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
 
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
-
-
-Route::get('/', function () {
-
-    return view('home');
-});
-
-//Index
-Route::get('/jobs', function () {
-
-    $jobs = Job::with('employer')->latest()->simplePaginate(10);
-    return view('jobs.index', [
-        'jobs' => $jobs
-    ]);
-});
-
-//Create
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-//Show
-Route::get('/jobs/{job}', function ($id) {
-    $job = Job::find($id);
-
-    return view('jobs.show', ['job' => $job]);
-});
-
-//Store
-Route::post('/jobs', function () {
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required'],
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1
-    ]);
-
-    return redirect('/jobs');
-});
-
-//Edit
-Route::get('/jobs/{job}/edit', function ($id) {
-    $job = Job::find($id);
-
-    return view('jobs.edit', ['job' => $job]);
-});
-
-//Update
-Route::patch('/jobs/{job}', function ($id) {
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required'],
-    ]);
-
-    $job = Job::findOrFail($id);
-
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary'),
-    ]);
-
-    return redirect('/jobs/' . $job->id);
-});
-
-//Destroy
-Route::delete('/jobs/{job}', function ($id) {
-
-    Job::findOrFail($id)->delete();
-
-    return redirect('/jobs');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::resource('jobs', JobController::class);
